230ac7fd766d9484b4c8208ba8be4b6b00efb6a8
Maximilian Neubert <m.neubert@mps-solutions.de>
2017-10-28 18:10:53 +0200

    Added databasemodel

---
+++ TestResult/Models/Model.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Die standardmäßige Codegenerierung für Modell 'c:\users\maximilian.neubert\documents\visual studio 2012\Projects\TestResult\TestResult\Models\Model.edmx' ist deaktiviert. 
+// Ändern Sie zur Aktivierung der standardmäßigen Codegenerierung den Wert der Designer-Eigenschaft 'Codegenerierungsstrategie'
+// in einen anderen Wert. Diese Eigenschaft ist im Fenster 'Eigenschaften' verfügbar, wenn das Modell
+// im Designer geöffnet ist.
