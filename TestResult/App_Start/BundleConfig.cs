47b41e55cafe2baa7d4c105f5322c36206a6bce8
Maximilian Neubert <m.neubert@mps-solutions.de>
2017-11-23 22:11:05 +0100

    Added licence and bundled javascript files

--- TestResult/App_Start/BundleConfig.cs
+++ TestResult/App_Start/BundleConfig.cs
@@ -8,36 +8,28 @@ namespace TestResult
         // Weitere Informationen zu Bundling finden Sie unter "http://go.microsoft.com/fwlink/?LinkId=254725".
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jsfiles").Include(
+                "~/scripts/angular.js",
+                "~/scripts/angular-route.js",
+                "~/scripts/jquery-3.2.1.js",
+                "~/scripts/dx.viz-web.debug.js"
+            ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/customscripts").Include(
+                "~/customscripts/databasesvc.js",
+                "~/customscripts/appareactrl.js",
+                "~/customscripts/runctrl.js",
+                "~/customscripts/gridutils.js",
+                "~/customscripts/formutils.js",
+                "~/customscripts/homectrl.js"
+            ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
-
-            // Verwenden Sie die Entwicklungsversion von Modernizr zum Entwickeln und Erweitern Ihrer Kenntnisse. Wenn Sie dann
-            // für die Produktion bereit sind, verwenden Sie das Buildtool unter "http://modernizr.com", um nur die benötigten Tests auszuwählen.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+            bundles.Add(new StyleBundle("~/content/cssfiles").Include(
+                "~/content/site.css",
+                "~/content/dx.spa.css",
+                "~/content/dx.common.css",
+                "~/content/dx.light.css"
+            ));
         }
     }
 }
